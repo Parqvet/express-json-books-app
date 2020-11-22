7eb400720a5c7eddd976d68472f24976e57747ad
Marcelo <marcelosen4@gmail.com>
2020-11-22 10:38:22 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+
+// routes
+
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).send("404 Not Found");
+})
+
+module.exports = app;
